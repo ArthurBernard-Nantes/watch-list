f42deb3d18c59e288b67c5fe7a7e1a9b67974b8c
arthur bernard <arthur.pierre.bernard@gmail.com>
2021-08-12 16:47:32 +0200

    manque redirect to new

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,34 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: [:create]
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(params_bookmark)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render 'lists/show'
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def params_bookmark
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
